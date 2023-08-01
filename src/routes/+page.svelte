e8a391bc3f6fbc501c25470ddcc61787f778fc2d
linushahs <suniltraveler2004@gmail.com>
2023-08-01 20:36:43 +0545

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section class="">
+	<h2>The Hacker News</h2>
+
+	<div>
+		<input type="text" name="" class="search-input" />
+		<button class="search-btn">Search</button>
+	</div>
+
+	<!-- List of news---- -->
+	<div class="news-container">
+		<div class="news-box">
+			<img src="" alt="img" class="news-poster" />
+			<h3>Title</h3>
+			<p>Description</p>
+			<button>Read more</button>
+		</div>
+	</div>
+</section>
+
+<style>
+	.search-input {
+		height: 25px;
+		width: 220px;
+	}
+
+	.search-btn {
+		padding: 0.4rem 1rem;
+	}
+
+	.news-container {
+		display: flex;
+		gap: 1.5rem;
+		margin: 2rem 0;
+	}
+	.news-box {
+		width: 300px;
+		height: fit-content;
+		border: 1px solid black;
+		border-radius: 8px;
+		padding: 1rem;
+	}
+
+	.news-poster {
+		width: 100%;
+		height: 80px;
+	}
+</style>
